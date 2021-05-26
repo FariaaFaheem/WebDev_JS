0de6127d9c590b9551210fa29fe711cf6451a1e5
FariaaFaheem <fariaa.faheem@gmail.com>
2021-05-26 10:50:46 +0500

    question 10 done

--- question10.js
+++ question10.js
@@ -1,3 +1,16 @@
 // question 10
 function isPrime(num){    
+    if (num > 1){
+        for (let i = 2; i < num; i++) {
+            if (num % i == 0) {
+                console.log(num + " is not a prime number.");
+                return false;
+            }
+        }  
+        console.log(num + " is a prime number.");
+        return true;
+   }
+   console.log(num + " is not a prime number.");
+   return false;
 }
+isPrime(23);
