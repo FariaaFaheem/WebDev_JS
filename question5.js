46e4b0aed64e92db58370e05149a5cbd0bd8aea8
FariaaFaheem <fariaa.faheem@gmail.com>
2021-05-30 21:44:51 +0500

    Final

--- question5.js
+++ question5.js
@@ -1,1 +1,8 @@
 // question 5
+function isRollNo (str){
+    var sub = str.slice(4,5);
+    if (Number(str.slice(0,3)) && Number(str.slice(6,8)) ) {
+        console.log("Good");
+    }
+}
+isRollNo("2019ee003");
